07e19fbe5589ec48235a5754507a2b9a05b2b0eb
hridi1912 <hriditaalam1@gmail.com>
2023-12-24 12:16:33 +0600

    1st commit

---
+++ lib/consts/list.dart
@@ -0,0 +1,23 @@
+import 'package:austmart/consts/consts.dart';
+
+const socialIconList=[
+  icFacebookLogo,
+  icGoogleLogo,
+  icTwitterLogo,
+];
+const sliderList=[
+  imgSlider1,
+  imgSlider2,
+  imgSlider3,
+  imgSlider4,
+];
+const secondsliderList=[
+  imgSs1,
+  imgSs2,
+  imgSs3,
+  imgSs4,
+];
+const profileButtonsList=[orders,wishlist,messages];
+const profileButtonsIcon=[icOrder,icOrder,icMessages];
+const catagoryList=[books,labMaterials,EngrDrawingMaterials,notes,stationary,semesterQues];
+const catagoryImages =[imgFc1,imgFc2,imgFc3,imgFc4,imgFc5,imgFc6,imgFc7,imgFc8,imgFc9];
